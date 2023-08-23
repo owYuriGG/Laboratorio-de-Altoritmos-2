d0a29675db42a04d6b844a6cb0614070f1c23336
Yuri Garcia <yurigamergarcia@gmail.com>
2023-08-23 09:04:52 -0300

    Update exercicioDojo1.py

--- semana03/exercicioDojo1.py
+++ semana03/exercicioDojo1.py
@@ -1,3 +1,6 @@
+# Faça um programa para gerar automaticamente números entre 0 e 99 de uma cartela de bingo. Sabendo que cada cartela devera conter 5 linhas de 5 números, 
+#gere estes dados de modo a não ter números repetidos dentro das cartelas. O programa deve exibir na tela a cartela gerada.
+
 import random
 
 def random_numbers(used):
