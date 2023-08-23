7e9f5c42977f0f9e9d7ecf8aa255410d75b58a6c
Yuri Garcia <yurigamergarcia@gmail.com>
2023-08-23 10:21:40 -0300

    Update inverterLista.py

--- semana02/ExerciciosParaEntregar/inverterLista.py
+++ semana02/ExerciciosParaEntregar/inverterLista.py
@@ -1,3 +1,6 @@
+# Você deve criar uma função chamada inverter_lista que aceita uma lista como entrada e retorna uma nova lista que é a inversão da lista original. 
+# Não é permitido o uso de métodos internos de reversão de listas, como reverse() ou slicing negativo. Você deve criar a lógica de inversão manualmente.
+
 def invert_list(list):
     reverse = []
     
