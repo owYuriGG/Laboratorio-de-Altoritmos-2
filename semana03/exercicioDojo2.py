4b824480cd63f63edf85aed5427db574ea32ad71
Yuri Garcia <yurigamergarcia@gmail.com>
2023-08-23 09:05:16 -0300

    Update exercicioDojo2.py

--- semana03/exercicioDojo2.py
+++ semana03/exercicioDojo2.py
@@ -1,3 +1,10 @@
+#  Desenvolva um programa que simule a entrega de notas quando um cliente efetuar um saque em um caixa eletrônico. Os requisitos básicos são os seguintes:
+# Entregar o menor número de notas;
+# É possível sacar o valor solicitado com as notas disponíveis;
+# Saldo do cliente infinito;
+# Quantidade de notas infinito (pode-se colocar um valor finito de cédulas para aumentar a dificuldade do problema);
+# Notas disponíveis de R$ 100,00; R$ 50,00; R$ 20,00 e R$ 10,00
+
 def verify_troco(saque):
     contador100 = 0
     contador50 = 0
