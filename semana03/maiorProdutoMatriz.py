bf0f0e58afd0e2af9e64f0d47c35b3b9411c7db3
Yuri Garcia <yurigamergarcia@gmail.com>
2023-08-26 12:01:11 -0300

    Update maiorProdutoMatriz.py

--- semana03/maiorProdutoMatriz.py
+++ semana03/maiorProdutoMatriz.py
@@ -10,26 +10,26 @@
 # 1 1 1 2 1
 # 1 1 1 1 2
 
-def sum(matrix):
-    n = len(matrix)
+def sum(matriz):
+    n = len(matriz)
     largest_product = 0
 
     # Verifica sequências horizontais
     for line in range(n):
         for colum in range(n - 4):
-            product = matrix[line][colum] * matrix[line][colum+1] * matrix[line][colum+2] * matrix[line][colum+3] * matrix[line][colum+4]
+            product = matriz[line][colum] * matriz[line][colum+1] * matriz[line][colum+2] * matriz[line][colum+3] * matriz[line][colum+4]
             largest_product = max(largest_product, product)
 
     # Verifica sequências verticais
     for line in range(n - 4):
         for colum in range(n):
-            product = matrix[line][colum] * matrix[line+1][colum] * matrix[line+2][colum] * matrix[line+3][colum] * matrix[line+4][colum]
+            product = matriz[line][colum] * matriz[line+1][colum] * matriz[line+2][colum] * matriz[line+3][colum] * matriz[line+4][colum]
             largest_product = max(largest_product, product)
 
     # Verifica sequências diagonais principais
     for line in range(n - 4):
         for colum in range(n - 4):
-            product = matrix[line][colum] * matrix[line+1][colum+1] * matrix[line+2][colum+2] * matrix[line+3][colum+3] * matrix[line+4][colum+4]
+            product = matriz[line][colum] * matriz[line+1][colum+1] * matriz[line+2][colum+2] * matriz[line+3][colum+3] * matriz[line+4][colum+4]
             largest_product = max(largest_product, product)
     return largest_product
 
